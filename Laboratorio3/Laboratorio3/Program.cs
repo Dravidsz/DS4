371650c396dd9dd24bab4155c475556502ce6a38
Dravidss <hdavidortega32@gmai.com>
2024-09-03 22:08:34 -0500

    Laboratorio # 3 De Desarrollo IV

---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero, suma;
+
+        Console.Write("Introcduce el primer numero");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo numero");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        suma = primerNumero + segundoNumero;
+
+        Console.WriteLine("Ls suma  de {0} y {1} es {2}", primerNumero, segundoNumero, suma);
+    }
+    }
+}
---
+++ Laboratorio3/Laboratorio31/Laboratorio31.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace laboratorio31
+{
+    internal class Laboratorio31
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, calcular;
+
+            Console.Write("Ingrese el primer valor  ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo valor  ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            calcular = (a + b) * (a - b);
+
+            Console.WriteLine("El resultado de la operacion (a+b) * (a-b) es: " + calcular);
+
+        }
+    }
+}
