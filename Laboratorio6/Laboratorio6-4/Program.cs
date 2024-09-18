cb9d178446bc321d3a95462e0b92d4b6345886cd
Héctor D. Ortega Q. <hdavidortega32@gmail.com>
2024-09-17 19:31:06 -0500

    Laboratorio 6 Desarrollo De Software

---
+++ Laboratorio6/Laboratorio6-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void chekAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso negado  no cumple con la edad suficiente");
+        }
+        else
+        {
+            Console.WriteLine("Acceso corecto");
+
+        }
+
+       
+    }
+    static void Main(string[] args)
+    {
+        chekAge(15);
+    }
+}
