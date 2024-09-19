aba1c2702fe04487ebc595b112a16a33f49a1c2e
Dravidsz <hdavidortga32@gmail.com>
2024-09-19 11:38:22 -0500

    laboratorio terminado

---
+++ Laboratorio8/Laboratorio8-8/ClaseAbstracta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8_8
+{
+     class ClaseAbstracta
+    {
+        abstract protected string tomaValor();
+        abstract public string prefixValor(string prefix);
+
+        public void printOut()
+        {
+            Console.WriteLine(tomaValor());
+        }
+    }
+}
---
+++ Laboratorio8/Laboratorio8-8/ClaseConcreta2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8_8
+{
+    class ClaseConcreta2 : ClaseAbstracta
+    {
+        protected override string tomaValor()
+        {
+            return "Clase Concreta 2";
+        }
+
+        public override string prefixValor(string prefix)
+        {
+            return $"{prefix} Clase Concreta 2";
+        }
+    }
+}
