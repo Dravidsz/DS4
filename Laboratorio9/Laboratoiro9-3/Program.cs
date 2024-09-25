163c135636f914c67e50f15726b5b2b5c5357360
Dravidsz <hdavidortga32@gmail.com>
2024-09-24 23:51:07 -0500

    Creacion de laboratorio #9

---
+++ Laboratorio9/Laboratoiro9-3/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Clasificación de triangulos");
+        Console.WriteLine("Clasificacione: Escaleno,Isósceles,Equilátero");
+        Console.WriteLine("Ingrese los tres lados para determinar que tipo de trinagulo es");
+
+        double ladoA, ladoB, ladoC;
+        Console.WriteLine("Ingrese el lado A");
+        ladoA = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el lado B");
+        ladoB = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el lado C");
+        ladoC = double.Parse(Console.ReadLine());
+        string tipoTriangulo = DeterminarTipoTriangulo(ladoA, ladoB, ladoC);
+        Console.WriteLine(tipoTriangulo);
+
+        static string DeterminarTipoTriangulo(double a, double b, double c)
+        {
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+
+                if (a == b && b == c)
+                {
+                    return "El triángulo es equilátero.";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    return "El triángulo es isósceles.";
+                }
+                else
+                {
+                    return "El triángulo es escaleno.";
+                }
+            }
+            else
+            {
+                return "Los lados ingresados no forman un triángulo.";
+            }
+        }
+    }
+}
---
+++ Laboratorio9/Laboratorio9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double precio, billetes, vuelto;
+        int formaPago;
+        string tarjeta;
+        do
+        {
+            Console.WriteLine("Ingrese el precio del producto(Que se valido)");
+            precio = Double.Parse(Console.ReadLine());
+           
+
+        } while (precio <= 0);
+
+        do
+        {
+            Console.WriteLine("Que forma de pago utilizara describa 1 para efectivo y 0 para targeta");
+            formaPago = int.Parse(Console.ReadLine());
+
+        } while (formaPago != 0 && formaPago != 1);
+
+        if(formaPago == 1)
+        {
+            Console.WriteLine("Ingrese la valor de billete");
+            billetes = Double.Parse(Console.ReadLine());
+            vuelto = billetes - precio;
+            Console.WriteLine("A pagado: " + precio + " y su vuelto es de " + vuelto);
+        } else if(formaPago == 0)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese el numero de targeta debe ser igual a 16 valores");
+                tarjeta = Console.ReadLine();
+
+            } while (tarjeta.Length != 16);
+            
+            
+            
+            Console.WriteLine("La targeta que ingreso es valida");
+            Console.WriteLine("A pagado con su tageta " + tarjeta + " el total de " + precio);
+        }
+        else
+        {
+            Console.WriteLine("Transacion invalida");
+        }
+
+      
+        
+
+
+
+    }
+
+
+}
